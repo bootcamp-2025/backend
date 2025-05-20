b8164ed38cee2413f697d10189db2e36950a2cc0
Mauricio <mau2606@gmail.com>
2025-05-20 19:07:12 -0400

    create movie

--- app/crud.py
+++ app/crud.py
@@ -1,13 +1,27 @@
 from app.models import Movie
 from typing import Optional
 
-async def get_movies(
+async def read_movies(
         skip:int=0,
         limit:int=10,
         title: Optional[str]=None,
         year: Optional[str]=None,
         director: Optional[str]=None
 ):
-
     query={}
+    if title: 
+        query["title"] = {"$regex": title, "$options": "i"}
+
+    if year: 
+        query["year"] = int(year)
+
+    if director: 
+        query["director"] = {"$regex": director, "$options": "i"}
+
     return await Movie.find(query).skip(skip).limit(limit).to_list()
+
+async def create_movie(
+       movie: Movie        
+)->Movie:
+    movie.id= None
+    return await movie.insert()
--- app/router.py
+++ app/router.py
@@ -1,17 +1,27 @@
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, Form
 from typing import Optional
 from app.models import Movie
-from app.crud import get_movies
+from app.crud import read_movies, create_movie
 
 router=APIRouter()
 
+@router.post("/movies", response_model=Movie)
+async def post_movie(
+        title: str = Form(None),
+        director: str | None = Form(None),
+        year: int | None = Form(None),
+        image_file: str | None = Form(None)
+):
+    movie = Movie(title=title, director=director, year=year)
+    return await create_movie(movie)
+
 @router.get("/movies", response_model=list[Movie])
-async def read_movies(
+async def get_movies(
         skip:int=Query(0, ge=0),
         limit:int=Query(10, le=100),
         title: Optional[str]=None,
-        year: Optional[str]=None,
+        year: Optional[int]=None,
         director: Optional[str]=None
 ):
 
-    return await get_movies(skip=skip, limit=limit, title=title, year=year, director=director)
+    return await read_movies(skip=skip, limit=limit, title=title, year=year, director=director)
